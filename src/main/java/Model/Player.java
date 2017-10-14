431fc368d467835a1859165d437a13ed0037076f
trexmix <elijahtrexler@gmail.com>
2017-10-14 11:11:44 -0400

    Player

---
+++ src/main/java/Model/Player.java
@@ -0,0 +1,41 @@
+public class Player {
+    int health;
+    int temp;
+    int thirst;
+    int energy;
+
+    int energyRate;
+
+    int locX;
+    int locY;
+
+    public Player() {
+        health = 100;
+        temp = 70;
+        thirst = 10;
+        energy = 100;
+
+        locX = 0;
+        locY = 0;
+    }
+
+    public void moveUp() {
+        locY++;
+        energy = energy - energyRate;
+    }
+
+    public void moveDown() {
+        locY--;
+        energy = energy - energyRate;
+    }
+
+    public void moveRight() {
+        locX++;
+        energy = energy - energyRate;
+    }
+
+    public void moveLeft() {
+        locY--;
+        energy = energy - energyRate;
+    }
+}
