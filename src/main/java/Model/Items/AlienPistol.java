d269cc1555cc68047b95522d7ad54d1d62a94a87
Daniel Greene <daniel.greene@gatech.edu>
2017-10-14 15:40:05 -0400

    Add more weapons

---
+++ src/main/java/Model/Items/AlienPistol.java
@@ -0,0 +1,9 @@
+package main.java.Model.Items;
+
+public class AlienPistol extends Weapon {
+
+    public AlienPistol(int damage, int range, int weight, String name) {
+        super(65, 2, 8,"Alien Pistol");
+    }
+}
+
---
+++ src/main/java/Model/Items/Slingshot.java
@@ -0,0 +1,8 @@
+package main.java.Model.Items;
+
+public class Slingshot extends Weapon {
+
+    public Slingshot(int damage, int range, int weight, String name) {
+        super(20, 2, 5,"Slingshot");
+    }
+}
