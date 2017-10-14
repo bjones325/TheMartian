6668ac137c945f342e460e8fea8ea23e08a64997
trexmix <elijahtrexler@gmail.com>
2017-10-14 16:03:11 -0400

    Fixed bug

--- src/main/java/Model/Tiles/BuildingManager.java
+++ src/main/java/Model/Tiles/BuildingManager.java
@@ -23,7 +23,7 @@ public class BuildingManager {
     public static BuildingManager getInstance() { return bm; }
 
     public void spawnBuilding(BuildingEnum buildingType, int posX, int posY)  {
-        Building b;
+        Building b = null;
 
 
         try {
@@ -36,7 +36,7 @@ public class BuildingManager {
             System.exit(23);
         }
 
-
+        buildingList.add(b);
 
     }
 
