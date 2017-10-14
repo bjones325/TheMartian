5b5d5bb5fa8983c66060f9d851a97a4d8f9e533a
trexmix <elijahtrexler@gmail.com>
2017-10-14 14:42:15 -0400

    fixed bug

--- src/main/java/Model/Tiles/BuildingManager.java
+++ src/main/java/Model/Tiles/BuildingManager.java
@@ -1,12 +1,15 @@
 package main.java.Model.Tiles;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BuildingManager {
     private List<Building> buildingList;
     private static BuildingManager bm = new BuildingManager();
 
-
+    private BuildingManager() {
+        buildingList = new ArrayList<Building>();
+    }
 
     public void onTick() {
         for (Building b : buildingList) {
