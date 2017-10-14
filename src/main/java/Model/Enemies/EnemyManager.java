7a70fd342cf61f7d38567a22ab38c0f40a342d62
Daniel Greene <daniel.greene@gatech.edu>
2017-10-14 12:35:03 -0400

    Make EnemyManager a singleton

--- src/main/java/Model/Enemies/EnemyManager.java
+++ src/main/java/Model/Enemies/EnemyManager.java
@@ -1,9 +1,11 @@
 package main.java.Model.Enemies;
+import main.java.Model.GameManager;
+
 import java.util.LinkedList;
 
 public class EnemyManager {
     LinkedList<Enemy> enemies;
-
+    private static EnemyManager instance = new EnemyManager();
     public void spawn(EnemyTypeEnum type, int locX, int locY ) {
         switch (type) {
             case MARTIANDOG : enemies.add(new MartianDog(locX,locY));
