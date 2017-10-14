f8da09997e372b180f8ccaf1fab9e94e3f218d70
Daniel Greene <daniel.greene@gatech.edu>
2017-10-14 19:11:55 -0400

    Art Changes

--- src/main/java/Model/Tiles/TileEnum.java
+++ src/main/java/Model/Tiles/TileEnum.java
@@ -10,12 +10,14 @@ public enum TileEnum {
     DESERT(new Image("File:./assets/desert.png", 53, 53, true, true)),
     LAVA(new Image("File:./assets/Lava.png", 53, 53, true, true)),
     SWAMP(new Image("File:./assets/Swamp.png", 53, 53, true, true)),
-    MOUNTAIN(new Image("File:./assets/Mountain.png", 53, 53, true, true)),
+    MOUNTAIN(new Image("File:./assets/mtns.png", 53, 53, true, true)),
     GAS_VENT(new Image("File:./assets/Gasvent.png", 53, 53, true, true)),
     CRATER(new Image("File:./assets/Crater.png", 53, 53, true, true)),
-    HILL(new Image("File:./assets/Ratios.png", 53, 53, true, true)),
-    IRON_VEIN(new Image("File:./assets/IronOreVain.png", 53, 53, true, true));
-
+    //HILL(new Image("File:./assets/Ratios.png", 53, 53, true, true)),
+    IRON_VEIN(new Image("File:./assets/IronOreVain.png", 53, 53, true, true)),
+    ALIEN_VEIN(new Image("File:./assets/AlienOreVain.png", 53, 53, true, true)),
+    ALUMINUM_VEIN(new Image("File:./assets/AluminumVain.png", 53, 53, true, true)),
+    URANIUM_VEIN(new Image("File:./assets/UraniumVain.png", 53, 53, true, true));
     private Image view;
 
     private TileEnum(Image iv) {
