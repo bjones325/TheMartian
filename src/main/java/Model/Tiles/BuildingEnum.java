f6ee4c03f89dbad8ec81d60de40548bb804575c5
trexmix <elijahtrexler@gmail.com>
2017-10-14 21:36:32 -0400

    Fixed horrendous pathfinding code. Embarrass how much work was requried to salvage it. Thank goodness he did not do the item code

--- src/main/java/Model/Tiles/BuildingEnum.java
+++ src/main/java/Model/Tiles/BuildingEnum.java
@@ -3,16 +3,17 @@ package main.java.Model.Tiles;
 import javafx.scene.image.Image;
 
 public enum BuildingEnum {
-    IRON_MINE(IronMine.class),
-    WELL(null),
-    TEMP_CONVERTER(null);
+    IRON_MINE(IronMine.class, new Image("File:./assets/IronMine.png")),
+    WELL(null, null),
+    TEMP_CONVERTER(null, null);
 
     private Class tmp;
 
     private Image buildingImg;
 
-    BuildingEnum(Class type) {
+    BuildingEnum(Class type, Image img) {
         this.tmp = type;
+        this.buildingImg = img;
     }
 
     public Class getRunnable() { return tmp; }
